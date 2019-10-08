7d06af458d292229ea9dd8f699c73d2e6141e1e1
antoboh26 <56228831+antoboh26@users.noreply.github.com>
2019-10-08 18:19:05 +0200

    Update produttore.java

--- produttore.java
+++ produttore.java
@@ -1,36 +1,37 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 // Produttore NON e' un filtro
-public class Produttore {
-	public static void main(String[] args) {		
-		
-		Scanner in = new Scanner(System.in);
-		if (args.length != 1){
-			System.out.println("Utilizzo: produttore <inputFilename>");
-			System.exit(0);
-		}
-		
-			
-		FileWriter fout;
-		try {
-			fout = new FileWriter(args[0]);
-			PrintWriter printW = new PrintWriter(fout);
-			String inputl = in.nextLine();
-			while(inputl != null){
-				printW.print(inputl);
-				System.out.println("Inserisci la nuova riga");
-				 inputl = in.nextLine();
-			}		
-			printW.close();
-		} 
-		catch (NumberFormatException nfe) { 
-			nfe.printStackTrace(); 
-			System.exit(1); // uscita con errore, intero positivo a livello di sistema Unix
-		}
-	    catch (IOException e) { 
-			e.printStackTrace();
-			System.exit(2); // uscita con errore, intero positivo a livello di sistema Unix
-		}
-	}
-}
+public class produttore {
+    public static void main(String[] args) {
+        BufferedReader in = null;
 
+        /*if (args.length != 1){
+            System.out.println("Utilizzo: produttore <inputFilename>");
+            System.exit(0);
+        }*/
+        in = new BufferedReader(new InputStreamReader(System.in));
+        FileWriter fout;
+        try {
+            fout = new FileWriter("ciao4.txt");
+            int n;
+            while ((n=in.read()) != -1) {
+                fout.write((char)n);
+                String inp = in.readLine()+"\n";
+                fout.write(inp, 0, inp.length());
+            }
+            fout.close();
+
+
+        } catch (NumberFormatException nfe) {
+            nfe.printStackTrace();
+            System.exit(1); // uscita con errore, intero positivo a livello di sistema Unix
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(2); // uscita con errore, intero positivo a livello di sistema Unix
+        }
+    }
+}
